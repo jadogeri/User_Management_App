290c7c66f12d222a6d283bc368b17e755e13384b
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-23 03:25:36 -0600

    Updated IUserRepository.interface.ts

---
+++ server/src/interfaces/IUserRepository.interface.ts
@@ -0,0 +1,8 @@
+
+import mongoose from "mongoose";
+
+export interface IUserRepository  {
+    findByName(name: string): Promise<any>;
+    findByEmail(email: string): Promise<any>;
+    findById(id: any): Promise<any>;
+}
