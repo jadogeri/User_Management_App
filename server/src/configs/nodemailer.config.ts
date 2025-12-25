1254394bc098a00c2780af7eecca655d1ea0aea1
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-24 20:40:43 -0600

    Updated nodemailer.config.ts

---
+++ server/src/configs/nodemailer.config.ts
@@ -0,0 +1,23 @@
+/**
+ * @author      Joseph Adogeri
+ * @since       24-DEC-2025
+ * @version     1.0
+ * @description configuration setting for nodemailer 
+ *  
+ */
+const creds = { user:process.env.NODEMAILER_USERNAME,
+    pass :process.env.NODEMAILER_PASSWORD};
+    
+export const inLineCss : Function = require('nodemailer-juice');
+export const nodemailer = require('nodemailer')
+export const transporter = nodemailer.createTransport({
+service: 'gmail',
+auth: {
+user: creds.user,
+pass: creds.pass,
+},
+}).use('compile', inLineCss());
+
+
+module.exports = {nodemailer, transporter}
+
