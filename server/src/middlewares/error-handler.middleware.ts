892af44751c36fd127b147fb9dea6014953d0c00
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-26 01:00:52 -0600

    Updated status codes in error handler

--- server/src/middlewares/error-handler.middleware.ts
+++ server/src/middlewares/error-handler.middleware.ts
@@ -1,51 +1,51 @@
-import { constants } from "../constants";
+import { STATUS_CODES} from "../constants/status-codes.constants";
 import { Response, Request,NextFunction } from "express";
 export const errorHandler = (err : Error, req : Request, res : Response, next : NextFunction) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
   switch (statusCode) {
-    case constants.VALIDATION_ERROR:
+    case STATUS_CODES.VALIDATION_ERROR:
       res.json({
         title: "Validation Failed",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-    case constants.NOT_FOUND:
+    case STATUS_CODES.NOT_FOUND:
       res.json({
         title: "Not Found",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-    case constants.UNAUTHORIZED:
+    case STATUS_CODES.UNAUTHORIZED:
       res.json({
         title: "Unauthorized",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-    case constants.FORBIDDEN:
+    case STATUS_CODES.FORBIDDEN:
       res.json({
         title: "Forbidden",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-    case constants.SERVER_ERROR:
+    case STATUS_CODES.SERVER_ERROR:
       res.json({
         title: "Server Error",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-    case constants.CONFLICT:
+    case STATUS_CODES.CONFLICT:
       res.json({
         title: "Conflict",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-    case constants.LOCKED_ACCOUNT:
+    case STATUS_CODES.LOCKED_ACCOUNT:
       res.json({
         title: "Locked account",
         message: err.message,
