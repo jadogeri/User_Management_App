53b77fb8068acd10de40856ba6191fed56ab6d6a
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-29 23:04:27 -0600

    Added morgan package for logging requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,9 @@ import * as swaggerJson from "./swagger.json";
 import * as swaggerUI from "swagger-ui-express";
 import cors from 'cors';
 import { corsOptions } from './configs/cors.config';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 
 dotenv.config();
@@ -29,8 +32,14 @@ export const buildApp = () : Application  =>{
     app.use(cors(corsOptions));
     // Enable pre-flight requests for all routes (necessary when using specific headers/methods)
     //app.options('*', cors(corsOptions) as any); 
+    const morganFormat = process?.env?.NODE_ENV === 'production' ? 'combined' : 'dev';
+    app.use(morgan(morganFormat));// Logging middleware     
+    // Create a write stream (in append mode)
+    console.log("__dirname:", __dirname);   
+    const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a' });
 
-   
+    // Setup the logger to use the 'combined' format and stream to the file
+    app.use(morgan('combined', { stream: accessLogStream }))  
 
     RegisterRoutes(app);
 
