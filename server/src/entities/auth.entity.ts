208e65e965275b6df852642ad0f03f9480f2346a
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-29 21:15:01 -0600

    Implemented application entities

---
+++ server/src/entities/auth.entity.ts
@@ -0,0 +1,24 @@
+/**
+ * @author Joseph Adogeri
+ * @version 1.0
+ * @since 28-DEC-2025
+ * @description class representing Auth entity;
+ */
+
+import { Entity, PrimaryGeneratedColumn, OneToOne, Column, JoinColumn } from "typeorm";
+import User from "./user.entity";
+import { AuthType } from "../types/auth.type";
+import { Audit } from "../models/audit.model";
+@Entity()
+export class Auth extends Audit implements AuthType {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', unique: true, nullable: true })
+    refreshToken: string;
+
+    @OneToOne(()=> User, { onDelete: 'CASCADE' })
+    @JoinColumn() 
+    user: User;
+
+}
---
+++ server/src/entities/user.entity.ts
@@ -0,0 +1,64 @@
+// src/entities/User.ts
+import { Entity,  Column, PrimaryGeneratedColumn, JoinColumn, ManyToOne } from "typeorm";
+import { Min, IsInt, IsString } from 'class-validator';
+import { Audit } from "../models/audit.model";
+import { UserType } from "../types/user.type";
+import Role from "./role.entity";
+import Status from "./status.entity";
+import { SuspensionDetails } from "../types/suspension-details.type";
+
+@Entity()
+export class User extends Audit implements UserType {
+  @PrimaryGeneratedColumn() // SQLite uses auto-increment integers by default
+  id: number;
+
+  @Column({type: "varchar", length: 40, nullable: false, unique: false })
+  @IsString()
+  fullname: string;
+
+  @Column({type: "varchar", length: 40, nullable: false, unique: true })
+  @IsString()
+  username: string;
+
+  @Column()
+  @IsString()
+  email: string;
+
+  @Column({type: "integer",default: 0, nullable: false, unsigned: true})
+  @IsInt() // Optional: ensures it's an integer
+  @Min(0, { message: 'Quantity cannot be a negative number' })
+  age: number;
+
+  @Column({type: "varchar", length: 15, nullable: true })
+  @IsString()
+  phone: string;
+
+  @Column({type: "varchar", length: 100, nullable: false })
+  @IsString()
+  password: string;
+
+  @Column({type: "integer", default: 0, nullable: false, unsigned: true })
+  @IsInt()
+  failedLogins: number;
+
+  @Column({type: "boolean", default: true, nullable: false })
+  @IsInt()
+  isEnabled: boolean; 
+
+    @Column({
+    type: "json", 
+    nullable: true,
+    default: null
+  })
+  suspension: SuspensionDetails | null;
+
+  @ManyToOne(() => Role)
+  @JoinColumn({ name: 'role_id' })
+  role: Role; // This will hold the Role object when loaded
+
+  @ManyToOne(() => Status)
+  @JoinColumn({ name: 'status_id' })
+  status: Status; // This will hold the Status object when loaded
+}
+
+export default User;
