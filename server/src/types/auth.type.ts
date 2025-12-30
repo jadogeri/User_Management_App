9706333956ea8802175ba405e1719ef56ff53dca
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-29 20:58:49 -0600

    Added user.type.ts

--- server/src/types/auth.type.ts
+++ server/src/types/auth.type.ts
@@ -1,6 +1,8 @@
 import User from "../entities/user.entity";
 
-export type AuthType =  {
+import { AuditType } from "./audit.type";
+
+export type AuthType = AuditType & {
   /**
    * The auth's identifier.
    * @example "1"
@@ -16,17 +18,6 @@ export type AuthType =  {
    * @example "{ id: 1, username: 'johndoe', email: 'johndoe@example.com' }"
    */
   user: User
-  /**
-   * The date and time when the auth was created.
-   * @format date-time
-   * @example "2024-01-01T12:00:00.000Z"
-   */
-  createdAt: Date;
-  /**
-   * The date and time when the auth was last updated.
-   * @format date-time
-   * @example "2024-01-01T13:00:00.000Z"
-   */
-  updatedAt: Date;
 
 }
+
---
+++ server/src/types/user.type.ts
@@ -0,0 +1,52 @@
+import { AuditType } from "./audit.type";
+
+export type UserType = AuditType & {
+    /**
+   * The user's identifier.
+   * @example "1"
+   */
+id: number
+  /**
+   * The user's full name.
+   * @example "John Doe"
+   */
+  fullname: string
+  /**
+   * The user's username.
+   * @example "John1Doe"
+   */
+  username: string
+  /**
+   * The user's email.
+   * @pattern ^(.+)@(.+)$ please provide correct email
+   * @example "JohnDoe@gmail.com"
+   */
+  email: string;
+  /**
+   * The user's age.
+   * @isInt we would kindly ask you to provide a number here
+   * @minimum 0 minimum age is 0
+   * @example 21
+   */
+  age: number;
+   /**
+   * The user's phone number.
+   * @example "123-456-7890"
+   */
+  phone: string;
+  /**
+   * The user's password.
+   * @example "SecurePassword123!"
+   */
+  password: string;
+    /**
+   * The user's failed login attempts.
+   * @example 0
+   */
+  failedLogins: number;
+    /**
+   * The user's account status.
+   * @example true
+   */
+  isEnabled: boolean;
+}
