7d7282adad4b2f7f3a86a8a249a5bcc31f54d72d
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-30 17:29:59 -0600

    Updated UserServiceInterface

--- server/src/interfaces/user-service.interface.ts
+++ server/src/interfaces/user-service.interface.ts
@@ -2,5 +2,10 @@
 
 
 export interface UserServiceInterface {
-
+  create(): Promise<any>;
+  getOne(): Promise<any>;
+  getAll(): Promise<any>;
+  modify(): Promise<any>;
+  reactivate(): Promise<any>;
+  deactivate( ): Promise<any> ;
 };
