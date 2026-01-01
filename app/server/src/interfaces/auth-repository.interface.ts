a0bffae537dd8f257ac59eb4c841881d9ca02d57
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-01 07:17:03 -0600

    updated auth repository interface

--- app/server/src/interfaces/auth-repository.interface.ts
+++ app/server/src/interfaces/auth-repository.interface.ts
@@ -1,12 +1,13 @@
 import { Repository } from "typeorm/repository/Repository";
 import { Auth } from "../entities/auth.entity";
+import User from "../entities/user.entity";
 
 export interface CustomAuthRepositoryMethodsInterface {
-  login(): Promise<any>;
-  logout(): Promise<any>;
-  forgot(): Promise<any>;
-  reset(): Promise<any>;
-  refresh(): Promise<any>;
+  findActiveAuths(): Promise<Auth[]>;
+  findByRefreshToken(refreshToken: string): Promise<Auth | null>;
+  findByUser(user: User): Promise<Auth | null>;
+  findByUserId(userId: number): Promise<Auth | null>;
+  findByRefreshToken(token: string): Promise<Auth | null>;
 }
 
 export interface AuthRepositoryInterface extends Repository<Auth>, CustomAuthRepositoryMethodsInterface {};
