46301df95045e368cad1b6d899d2028e2fa0f654
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-31 19:56:52 -0600

    Added more functions to bcrypt service interface

---
+++ server/src/interfaces/bcrypt-service.interface.ts
@@ -0,0 +1,14 @@
+
+export interface BcryptServiceServiceInterface {
+  
+  updateUUID(): void;
+
+  getUUID(): string;
+
+  generateUUID(): string;
+
+  generateHashedPassword(plainTextPassword: string): Promise<string>;
+
+  comparePassword(plainTextPassword: string, hashedPassword: string): Promise<boolean>;
+
+}
