fb36eb1cd915cab393f2d92919992638b383f7f3
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-01 01:31:35 -0600

    Updated Role class

--- app/server/src/entities/role.entity.ts
+++ app/server/src/entities/role.entity.ts
@@ -4,9 +4,10 @@
  * @description class representing Role entity;
  */
 
-import { Entity, PrimaryGeneratedColumn, Column, Check, OneToMany } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, Check, JoinTable, ManyToMany } from 'typeorm';
 import { RoleNamesEnum } from '../types/role-names.type';
 import User from './user.entity';
+import { Permission } from './permission.entity';
 
 @Entity('role')
 export class Role {
@@ -18,6 +19,8 @@ export class Role {
     type: 'simple-enum',    
     unique: true,
     length: 20,
+    nullable: false,
+    enum: RoleNamesEnum,
     default: RoleNamesEnum.USER,
   })
   name: RoleNamesEnum;  
@@ -30,12 +33,25 @@ export class Role {
   })  
   description: string;  
 
-  @OneToMany(() => User, (user) => user.role)
+  @ManyToMany(() => Permission, permission => permission.roles)
+  @JoinTable({
+    name: 'role_permissions', // Custom name for the join table
+    joinColumn: { name: 'role_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'permission_id', referencedColumnName: 'id' },
+  })
+  permissions: Permission[];
+
+  @ManyToMany(() => User, user => user.roles)
   users: User[];
+
+  @ManyToMany(() => Group, group => group.roles)
+  groups: Group[];
 
 }
 
 export default Role;
+
+
 
 
 
