84da4af8a88ee3d8dc1f66088d802b7523e0c236
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-01 06:52:14 -0600

    Updated auth controller

--- app/server/src/controllers/auth.controller.ts
+++ app/server/src/controllers/auth.controller.ts
@@ -1,11 +1,15 @@
 
 import { Request, Controller as BaseController, Body, Delete, Get, Post, Put, Route, Tags, Response, Path, Example, SuccessResponse, Res, TsoaResponse, Security} from "tsoa";
 import { AutoWired, Controller, Middleware } from "../decorators";
+import { Request as ExpressRequest } from "express";
 
 import { TYPES } from "../types/binding.type";
 import { AuthControllerInterface } from "../interfaces/auth-controller.interface";
 import { AuthServiceInterface } from "../interfaces/auth-service.interface";
 import loginLimitterMiddleware from "../middlewares/login-limitter.middleware";
+import { AuthLoginRequestDTO } from "../dtos/requests/auth-request.dto";
+import { AuthLoginResponseDTO } from "../dtos/responses/auth-response.dto";
+import { ErrorResponse } from "../models/error-response.model";
 
 
 @Route("auths")
@@ -30,11 +34,10 @@ export class AuthController extends BaseController implements AuthControllerInte
   }
 
   @Post("/login")
-  @Middleware(loginLimitterMiddleware)
-  public async loginUser(): Promise<any> {
-    return this.authService.login();
+  public async loginUser(@Body() userRequest: AuthLoginRequestDTO, @Request() req: ExpressRequest): Promise<AuthLoginResponseDTO | ErrorResponse> {
+    return this.userService.login(userRequest, req);
+  }
 
-  }  
   @Post("/logout")
   public async logoutUser(): Promise<any> {
     return this.authService.logout();
