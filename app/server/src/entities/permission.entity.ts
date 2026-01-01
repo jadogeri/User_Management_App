c1d5c21134fe881c4ea06ec26dba3d080e1cab56
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-01 01:23:22 -0600

    Updated permission class

--- app/server/src/entities/permission.entity.ts
+++ app/server/src/entities/permission.entity.ts
@@ -1,13 +1,25 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
 import Role from './role.entity';
+import { PermissionNamesEnum } from '../types/permission-names.type';
+import { IsEnum } from 'class-validator';
+import { PermissionType } from '../types/permission.type';
 
 @Entity()
-export class Permission {
+export class Permission implements PermissionType{
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true })
-  name: string; // e.g., 'edit_posts'
+  @Column({
+    type: 'simple-enum',  
+    enum: PermissionNamesEnum,  
+    unique: true,
+    //length: 20,
+  })
+  @IsEnum(PermissionNamesEnum, { message: 'name must be a valid enum value (DELETE_USER, READ_USER).' })
+  name: PermissionNamesEnum;
+
+  @Column({ nullable: true })
+  description: string;
 
   @ManyToMany(() => Role, (role) => role.permissions)
   roles: Role[];
