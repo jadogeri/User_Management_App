d387a955636077360a1129a3c130425068251003
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-12-31 19:21:02 -0600

    Added role.seed.ts and status.seed.ts

---
+++ server/src/database/seeds/role.seed.ts
@@ -0,0 +1,28 @@
+// src/database/seeds/RoleSeeder.ts
+import { DataSource } from 'typeorm';
+import { Seeder } from 'typeorm-extension';
+import { Role } from '../../entities/role.entity';
+import { AdminRole, UserRole, ViewerRole, EditorRole } from '../../constants/role.constant';
+export default class RoleSeeder implements Seeder {
+/**
+   * Checks for the existence of predefined status entities in the database 
+   * and saves them if they do not already exist. 
+   * 
+   * @param dataSource - The data source used to access the repository.
+   * @returns A promise that resolves to void.
+   * @throws Any errors thrown by the repository methods during execution.
+   */
+  public async run(dataSource: DataSource): Promise<void> {
+    const roleRepository = dataSource.getRepository(Role);
+    const roles = [AdminRole, UserRole, ViewerRole, EditorRole];
+
+    for (const role of roles) {
+      // Check if the role already exists by ID
+      const existingRole = await roleRepository.findOneBy({ id: role.id });
+      
+      if (!existingRole) {
+        await roleRepository.save(role);
+      }
+    }
+  }
+}
---
+++ server/src/database/seeds/status.seed.ts
@@ -0,0 +1,29 @@
+// src/database/seeds/StatusSeeder.ts
+import { DataSource } from 'typeorm';
+import { Seeder } from 'typeorm-extension';
+import { Status } from '../../entities/status.entity';
+import { EnabledStatus, DisabledStatus, LockedStatus } from '../../constants/status.constant';
+
+export default class StatusSeeder implements Seeder {
+/**
+   * Checks for the existence of predefined status entities in the database 
+   * and saves them if they do not already exist. 
+   * 
+   * @param dataSource - The data source used to access the repository.
+   * @returns A promise that resolves to void.
+   * @throws Any errors thrown by the repository methods during execution.
+   */
+  public async run(dataSource: DataSource): Promise<void> {
+    const statusRepository = dataSource.getRepository(Status);
+    const statuses = [EnabledStatus, DisabledStatus, LockedStatus];
+    
+    for (const status of statuses) {
+      // Check if the status already exists by ID
+      const existingStatus = await statusRepository.findOneBy({ id: status.id });
+      
+      if (!existingStatus) {
+        await statusRepository.save(status);
+      }
+    }
+  }
+}
