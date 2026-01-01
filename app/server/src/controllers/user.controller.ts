1741122c8c9321ace27656a933880e25ce498945
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-01 04:35:35 -0600

    Added credential validator service interface

--- app/server/src/controllers/user.controller.ts
+++ app/server/src/controllers/user.controller.ts
@@ -7,6 +7,8 @@ import loginLimitterMiddleware from "../middlewares/login-limitter.middleware";
 import { UserControllerInterface } from "../interfaces/user-controller.interface";
 import { UserServiceInterface } from "../interfaces/user-service.interface";
 import { logger } from "../configs/logger.config";
+import { UserCreateResponseDTO } from "../dtos/responses/user-response.dto";
+import { UserCreateRequestDTO } from "../dtos/requests/user-request.dto";
 
 
 @Route("users")
@@ -18,16 +20,44 @@ export class UserController extends BaseController implements UserControllerInte
   private readonly userService!: UserServiceInterface;
 
 
-    /**
+/**
    * Creates a new user in the system.
    * @summary Create a new user
    * @param requestBody The user details for creation.
    * @returns The newly created user.
    */
   @SuccessResponse("201", "Created")
-  @Post("/create")
-  public async createUser(): Promise<any> {
-    return this.userService.create();
+  @Example<UserCreateResponseDTO>({
+    id: 1,
+    username: "john1doe",
+    fullname: "John Doe",
+    email: "johndoe@tsoa.com",
+    phone: "123-456-7890",
+    age: 21,
+    createdAt: new Date("2023-01-01T10:00:00Z"),
+    updatedAt: new Date("2023-01-01T11:30:00Z"),
+    failedLogins: 0,
+    isEnabled: false
+  })  
+  @Post("/register")   
+  public async registerUser( @Body() requestBody: UserCreateRequestDTO): Promise<UserCreateResponseDTO |  ErrorResponse> {
+
+
+    const userRequest : UserCreateRequestDTO = requestBody
+    // calling validation service
+    const validation : ValidationResponse = this.credentialValidatorService.validateRegistration(userRequest);
+    if(!validation.isValid()){
+      console.log("validation failed in controller", validation.getErrorResponse()?.getMessage());
+      const errorResponse : ErrorResponse = validation.getErrorResponse() as ErrorResponse;
+      return errorResponse;
+      //errorBroadcaster(res,errorResponse.getCode(), errorResponse.getMessage())
+    }   
+    //calling user service
+    const userResponse : ErrorResponse | UserRegisterResponseDTO = await this.userService.register(userRequest);    
+
+    return userResponse;
+
+
   }
 
   @Get("/get-one")
