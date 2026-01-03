38a291130082f93ae59da065c67e407642ab6387
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-03 00:37:45 -0600

    Updated access control interface

--- app/server/src/interfaces/access-control.interface.ts
+++ app/server/src/interfaces/access-control.interface.ts
@@ -7,5 +7,8 @@ export interface AccessControlInterface{
   can(action: Action, resource: Resource): boolean ;
   hasRole(role: Role ): boolean ;
   hasFullAccess(grants: {resource: string, action: string}[]): boolean ;    
+  getGrants(): {resource: string, action: string}[] ;
+  setUserRoles(roles: Role[]): void ;    
 
 };
+
