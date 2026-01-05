6dcb3465f15ac306564477b202850b9560467856
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-01-04 23:11:52 -0600

    Added ContactType

---
+++ app/server/src/types/contact.type copy.ts
@@ -0,0 +1,33 @@
+import { AuditType } from "./audit.type";
+
+export type ContactType = AuditType & {
+    /**
+   * The contact's identifier.
+   * @example "1"
+   */
+id: number
+  /**
+   * The contact's full name.
+   * @example "John Doe"
+   */
+  fullname: string
+  /**
+   * The contact's email.
+   * @pattern ^(.+)@(.+)$ please provide correct email
+   * @example "JohnDoe@gmail.com"
+   */
+  email: string;
+   /**
+   * The contact's phone number.
+   * @example "123-456-7890"
+   */
+  phone: string;
+    /**
+   * The contact's fax number.
+   * @example "123-456-7890"
+   */
+
+  fax?: string;
+
+
+}
